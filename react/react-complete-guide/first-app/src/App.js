e11bc56a0de3111172c2eda4dced694c50cc3809
Jacky Xu <jackyxu07@gmail.com>
2019-02-01 00:25:42 -0500

    [react-complete-guide] lessons on injecting css

--- react/react-complete-guide/first-app/src/App.js
+++ react/react-complete-guide/first-app/src/App.js
@@ -34,13 +34,24 @@ class App extends Component {
   };
 
   render() {
+    // inline styles in javascript, style is scoped to component
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
 
         {/* Note: in JSX the click handler is onClick with a capital C!*/}
-        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
 
         <Person
           name={this.state.persons[0].name}
--- react/react-complete-guide/first-app/src/Person/Person.js
+++ react/react-complete-guide/first-app/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css'; // webpack can handle importing of stylesheets, style is scoped globally
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I'm a Person and I am {props.name} {props.age} old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
