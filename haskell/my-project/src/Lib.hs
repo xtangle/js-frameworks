222c99dcb556b32e51d8dc4a2bfa2e4a43c2fd18
Jacky Xu <jackyxu07@gmail.com>
2019-09-02 15:14:37 -0400

    formatted code

--- haskell/my-project/src/Lib.hs
+++ haskell/my-project/src/Lib.hs
@@ -3,13 +3,23 @@ module Lib
     ) where
 
 readInts :: String -> [Int]
-readInts s = let ws = words s in map read ws
+readInts s =
+    let ws = words s
+     in map read ws
 
 minMax :: Ord a => [a] -> Maybe (a, a)
-minMax (h : t) = Just $ foldr
-    (\x (min, max) -> (if x < min then x else min, if x > max then x else max))
-    (h, h)
-    t
+minMax (h:t) =
+    Just $
+    foldr
+        (\x (min, max) ->
+             ( if x < min
+                   then x
+                   else min
+             , if x > max
+                   then x
+                   else max))
+        (h, h)
+        t
 minMax _ = Nothing
 
 printStats :: IO ()
