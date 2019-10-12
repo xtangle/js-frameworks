1da8bf5b7a619dd03ee82b1b515f95789a25691e
Jacky Xu <jackyxu07@gmail.com>
2019-10-12 16:10:36 -0400

    fix modules

--- haskell/playground/modules/Geometry/Cube.hs
+++ haskell/playground/modules/Geometry/Cube.hs
@@ -1,12 +1,12 @@
-module Geometry.Cube  
-( volume  
-, area  
-) where  
-  
-import qualified Geometry.Cuboid as Cuboid  
-  
-volume :: Float -> Float  
-volume side = Cuboid.volume side side side  
-  
-area :: Float -> Float  
-area side = Cuboid.area side side side 
+module Modules.Geometry.Cube
+    ( volume
+    , area
+    ) where
+
+import qualified Modules.Geometry.Cuboid as Cuboid
+
+volume :: Float -> Float
+volume side = Cuboid.volume side side side
+
+area :: Float -> Float
+area side = Cuboid.area side side side
--- haskell/playground/modules/Geometry/Sphere.hs
+++ haskell/playground/modules/Geometry/Sphere.hs
@@ -1,10 +1,10 @@
-module Geometry.Sphere  
-( volume  
-, area  
-) where  
-  
-volume :: Float -> Float  
-volume radius = (4.0 / 3.0) * pi * (radius ^ 3)  
-  
-area :: Float -> Float  
-area radius = 4 * pi * (radius ^ 2)  
+module Modules.Geometry.Sphere
+    ( volume
+    , area
+    ) where
+
+volume :: Float -> Float
+volume radius = (4.0 / 3.0) * pi * (radius ^ 3)
+
+area :: Float -> Float
+area radius = 4 * pi * (radius ^ 2)
